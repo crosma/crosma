583212cdebd413f13c05d5c5ef67f8d82c9ca579
Matthew Crossley <vbaspcppguy@gmail.com>
2012-07-03 14:18:17 -0400

    No cloudfront.

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ if (process.env.CROSMA_ENV == 'production')
 
 	conf.site_name = 'crosma.us';
 
-	conf.static_url = 'http://cf.crosma.us';
+	conf.static_url = 'http://static.crosma.us';
 	conf.cache_static = true;
 	
 	conf.cache_views = true;
