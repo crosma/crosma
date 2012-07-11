791db2c875918d9bb2f2bdb857ff1a26c369434c
Matthew Crossley <vbaspcppguy@gmail.com>
2012-07-11 01:36:30 -0400

    Speed testing

--- admin/controllers/mongo.js
+++ admin/controllers/mongo.js
@@ -15,13 +15,73 @@ page.handles('/mongo', 'get', function(req, res, next) {
 	async.parallel([
 		function(callback){
 			mdb.schema.User.count({}, function (err, count) {
-				counts.push(['User', count]);
+				counts.push(['User1', count]);
+				callback(err);
+			});
+		},
+		function(callback){
+			mdb.schema.User.count({}, function (err, count) {
+				counts.push(['User2', count]);
+				callback(err);
+			});
+		},
+		function(callback){
+			mdb.schema.User.count({}, function (err, count) {
+				counts.push(['User3', count]);
+				callback(err);
+			});
+		},
+		function(callback){
+			mdb.schema.User.count({}, function (err, count) {
+				counts.push(['User4', count]);
+				callback(err);
+			});
+		},
+		function(callback){
+			mdb.schema.User.count({}, function (err, count) {
+				counts.push(['User5', count]);
+				callback(err);
+			});
+		},
+		function(callback){
+			mdb.schema.User.count({}, function (err, count) {
+				counts.push(['User6', count]);
 				callback(err);
 			});
 		},
 		function(callback){
 			mdb.schema.Post.count({}, function (err, count) {
-				counts.push(['Post', count]);
+				counts.push(['Post1', count]);
+				callback(err);
+			});
+		},
+		function(callback){
+			mdb.schema.Post.count({}, function (err, count) {
+				counts.push(['Post2', count]);
+				callback(err);
+			});
+		},
+		function(callback){
+			mdb.schema.Post.count({}, function (err, count) {
+				counts.push(['Post3', count]);
+				callback(err);
+			});
+		},
+		function(callback){
+			mdb.schema.Post.count({}, function (err, count) {
+				counts.push(['Post4', count]);
+				callback(err);
+			});
+		},
+		function(callback){
+			mdb.schema.Post.count({}, function (err, count) {
+				counts.push(['Post5', count]);
+				callback(err);
+			});
+		},
+		function(callback){
+			mdb.schema.Post.count({}, function (err, count) {
+				counts.push(['Post6', count]);
 				callback(err);
 			});
 		},
