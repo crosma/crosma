6ca530b09bd5bc0caaa713166b9ed9e1ad8e4ad0
Matthew Crossley <vbaspcppguy@gmail.com>
2012-07-18 05:13:02 -0400

    .

--- admin/controllers/aws/sns.js
+++ admin/controllers/aws/sns.js
@@ -3,6 +3,7 @@ var	 app = require('../../../app')
 	,crypto = require('crypto')
 	,mdb = require('mongoose')
 	,https = require('https')
+	,url = require('url')
 	,page = require('../../../lib/controller')(app.servers.admin, {
 		 title: 'AWS SNS Incomming'
 		,require_auth: false
@@ -38,13 +39,24 @@ function handle(body, req, res, next) {
 		console.log('Subscribing to ' + body.TopicArn);
 		console.log('body.SubscribeURL = ' + body.SubscribeURL);
 		
-		https.get(body.SubscribeURL, function(r){
-		  console.log("statusCode: ", res.statusCode);
-		  console.log("headers: ", res.headers);
+		var options = url.parse(body.SubscribeURL);
+		/*
+		var options = {
+			hostname: u.hostname,
+			port: 443,
+			path: '/',
+			method: 'GET'
+		};
+		*/
 
-		  r.on('data', function(d) {
-			process.stdout.write(d);
-		  });
+		var the_req = https.request(options, function(res) {
+			//https.get(body.SubscribeURL, function(the_res){
+			console.log("statusCode: ", res.statusCode);
+			console.log("headers: ", res.headers);
+
+			the_res.on('data', function(d) {
+				process.stdout.write(d);
+			});
 		  
 		}).on('error', function(e) {
 			console.error(e);
